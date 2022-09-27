9cca72e4b78b88cda31ccffddbee43c9aa68cec7
unknown <sergey.borovs@gmail.com>
2022-09-27 16:58:30 +0300

    Добавил решенные задачи

---
+++ wait.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    wait = WebDriverWait(browser, 20).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), "100")
+        )
+    button = browser.find_element(By.ID, "book")
+    button.click()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    answer = browser.find_element(By.ID, "answer")
+    answer.send_keys(y)
+    button = browser.find_element(By.CSS_SELECTOR, "[type = 'submit']")
+    button.click()
+finally:
+    time.sleep(7)
+    browser.quit()
